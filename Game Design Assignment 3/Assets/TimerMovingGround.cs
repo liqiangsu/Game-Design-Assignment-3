68240d94dbf8c60c0e12534b38c447cf3bec60eb
sudevil <liqiang.su.89@gmail.com>
2015-05-04 16:32:26 +1000

    add missing files for last commit

---
+++ Game Design Assignment 3/Assets/TimerMovingGround.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimerMovingGround : MonoBehaviour
+{
+    public float IntervalTimeInSecond = 3;
+
+    public bool IsStoped;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Move()
+    {
+        
+    }
+}
