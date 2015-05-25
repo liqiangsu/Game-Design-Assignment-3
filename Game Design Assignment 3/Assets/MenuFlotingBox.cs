2ba3fa8742de152b9886003f93107d8cd0816cab
DannySu <liqiang.su.89@gmail.com>
2015-05-25 21:15:01 +1000

    New Menu sences

---
+++ Game Design Assignment 3/Assets/MenuFlotingBox.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuFlotingBox : MonoBehaviour {
+
+    [SerializeField]
+    float DelayOffset;
+    [SerializeField]
+    float Magnitute;
+    [SerializeField]
+    Vector3 offSet;
+    [SerializeField]
+    float delay = 0;
+
+    Vector3 orgPosition;
+    Vector3 targetPosition;
+
+    bool moveingToTarget;
+	// Use this for initialization
+	void Start () {
+        orgPosition = transform.position;
+        delay = Random.insideUnitSphere.magnitude * DelayOffset;
+        targetPosition = orgPosition + offSet;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Time.time > delay){
+            if(transform.position == orgPosition){
+                moveingToTarget = true;
+            }
+            if(transform.position == targetPosition){
+                moveingToTarget = false;
+            }
+
+            if (moveingToTarget)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetPosition, Magnitute);
+            }else{
+                transform.position = Vector3.MoveTowards(transform.position, orgPosition, Magnitute);
+            }
+        }
+
+
+	}
+}
---
+++ Game Design Assignment 3/Assets/MenuStartCube.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuStartCube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Application.LoadLevel("Map");
+        }
+    }
+}
