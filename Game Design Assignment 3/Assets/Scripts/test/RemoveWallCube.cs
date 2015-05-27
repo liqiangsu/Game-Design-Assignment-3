ee29486dc662cc3424df1715750dcf79bc3c8f33
kylinchen921 <foavin01@qq.com>
2015-05-27 19:40:39 +1000

    add lvl3 things

---
+++ Game Design Assignment 3/Assets/Scripts/test/RemoveWallCube.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveWallCube : MonoBehaviour {
+
+	[SerializeField]
+	private string cubeName;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
