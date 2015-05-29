36ed95003af14fc9a6e5c87577a68b8fc9dd0ce1
kylinchen921 <foavin01@qq.com>
2015-05-29 14:56:21 +1000

    Change LVL3 Style

--- Game Design Assignment 3/Assets/Scripts/test/ShaderChange.cs
+++ Game Design Assignment 3/Assets/Scripts/test/ShaderChange.cs
@@ -7,21 +7,20 @@ public class ShaderChange : MonoBehaviour {
 	private Material transparentGround;
 	private Material normalGround;
 	void Start () {
-		transparentGround = GameObject.Find ("GroundCube 8").GetComponent<Renderer>().material;
-		normalGround = GameObject.Find ("GroundCube 3").GetComponent<Renderer>().material;
+		transparentGround = GameObject.Find ("s1_hGroundTransparent 1").GetComponent<Renderer>().material;
+		normalGround = GameObject.Find ("s1_hGround 1").GetComponent<Renderer>().material;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
-
+	
 	void OnCollisionEnter(Collision collisionInfo) {
 		if(collisionInfo.gameObject.name.Equals("Player")){
-			GameObject[] tops = GameObject.FindGameObjectsWithTag("Top");
+			GameObject[] tops = GameObject.FindGameObjectsWithTag("LVL3_Top");
 			for(int i = 0; i < tops.Length; i ++) {
 				GameObject go = tops[i];
-				Debug.Log(go.name);
 				//go.GetComponent<Renderer>().material.mainTexture = Resources.Load("Materials/Wall") as Texture;
 				//go.GetComponent<Renderer>().material = AssetDatabase.LoadAssetAtPath("Assets/Materials/Transparent", typeof(Material))) as Material;
 				//go.GetComponent<Renderer>().material = AssetDatabase.LoadAssetAtPath("Assets/Materials/Transparent", typeof(Material)) as Material;
@@ -29,13 +28,13 @@ public class ShaderChange : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	void OnCollisionExit(Collision collisionInfo) {
 		if(collisionInfo.gameObject.name.Equals("Player")){
-			GameObject[] tops = GameObject.FindGameObjectsWithTag("Top");
+			GameObject[] tops = GameObject.FindGameObjectsWithTag("LVL3_Top");
 			for(int i = 0; i < tops.Length; i ++) {
 				GameObject go = tops[i];
-//				Debug.Log(go.name);
+				//				Debug.Log(go.name);
 				//go.GetComponent<Renderer>().material.mainTexture = Resources.Load("Materials/Wall") as Texture;
 				//go.GetComponent<Renderer>().material = AssetDatabase.LoadAssetAtPath("Assets/Materials/Transparent", typeof(Material))) as Material;
 				//go.GetComponent<Renderer>().material = AssetDatabase.LoadAssetAtPath("Assets/Materials/Transparent", typeof(Material)) as Material;
