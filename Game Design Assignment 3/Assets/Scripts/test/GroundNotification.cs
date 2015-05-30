63361c24d9dc20a8baae1bcfa0d07106f4cae9eb
kylinchen921 <foavin01@qq.com>
2015-05-30 12:47:06 +1000

    add high light function for next ground

---
+++ Game Design Assignment 3/Assets/Scripts/test/GroundNotification.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundNotification : MonoBehaviour {
+	
+	[SerializeField]
+	private GameObject lightingObject;
+	[SerializeField]
+	private float Delay = 0.5f;
+	[SerializeField]
+	private int times = 5;
+
+
+	IEnumerator OnCollisionEnter(Collision collisionInfo) {
+		if(collisionInfo.gameObject.name.Equals("Player")) {
+			GameObject light = GameObject.Find ("TargetLight");
+			Vector3 targetPos = new Vector3();
+			targetPos.x = lightingObject.transform.position.x;
+			targetPos.z = lightingObject.transform.position.z;
+			targetPos.y = light.transform.position.y;
+
+			Vector3 hiddenPos = targetPos;
+			hiddenPos.y -= 10;
+			for(int i = 0; i < times; i ++) {
+				light.transform.position = targetPos;
+				yield return new WaitForSeconds(Delay);
+				light.transform.position = hiddenPos;
+				yield return new WaitForSeconds(Delay);
+			}
+
+		}
+	}
+
+}
